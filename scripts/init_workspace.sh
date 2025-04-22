9b01d9a025b2091912e9c7a1d1ab9f118888fd14
Lee (李) <affirm-bats-yodel@duck.com>
2025-04-22 11:03:06 +0900

    fix(scripts/init_workspace.sh): patch to get directory from argument
      * change CRLF -> LF

--- scripts/init_workspace.sh
+++ scripts/init_workspace.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+TARGET_DIR="$1"
+
 # Check $WORKSPACE_DIRECTORY exists and execute command to
 # chown $WORKSPACE_DIRECTORY to 1000:1000 (node:node)
-if [ -d "${WORKSPACE_DIRECTORY}" ]; then
-  echo "execute: chown -R 1000:1000 ${WORKSPACE_DIRECTORY}"
-  chown -R node:node ${WORKSPACE_DIRECTORY}
+if [ -d "${TARGET_DIR}" ]; then
+  echo "execute: chown -R 1000:1000 ${TARGET_DIR}"
+  chown -R node:node ${TARGET_DIR}
+else
+  echo "Directory does not exist: ${TARGET_DIR}" >&2
+  exit 1
 fi
